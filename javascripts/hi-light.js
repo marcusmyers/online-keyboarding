cdefd0215f680dfe2d1b0f3e79d93b2f06e2de23
Mark Myers <marcusmyers@gmail.com>
2013-01-22 23:38:29 -0500

    Add all the demo files need for site to work

---
+++ javascripts/hi-light.js
@@ -0,0 +1,708 @@
+// Alphabet
+KeyboardJS.on('a', function(){
+	$('#a').css('background', 'green');
+},
+function() {
+	$('#a').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('b', function(){
+	$('#b').css('background', 'green');
+},
+function() {
+	$('#b').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('c', function(){
+	$('#c').css('background', 'green');
+},
+function() {
+	$('#c').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('d', function(){
+	$('#d').css('background', 'green');
+},
+function() {
+	$('#d').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('e', function(){
+	$('#e').css('background', 'green');
+},
+function() {
+	$('#e').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('f', function(){
+	$('#f').css('background', 'green');
+},
+function() {
+	$('#f').css('background', '#ffffff');
+}
+);
+
+
+KeyboardJS.on('g', function(){
+	$('#g').css('background', 'green');
+},
+function() {
+	$('#g').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('h', function(){
+	$('#h').css('background', 'green');
+},
+function() {
+	$('#h').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('i', function(){
+	$('#i').css('background', 'green');
+},
+function() {
+	$('#i').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('j', function(){
+	$('#j').css('background', 'green');
+},
+function() {
+	$('#j').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('k', function(){
+	$('#k').css('background', 'green');
+},
+function() {
+	$('#k').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('l', function(){
+	$('#l').css('background', 'green');
+},
+function() {
+	$('#l').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('m', function(){
+	$('#m').css('background', 'green');
+},
+function() {
+	$('#m').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('n', function(){
+	$('#n').css('background', 'green');
+},
+function() {
+	$('#n').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('o', function(){
+	$('#o').css('background', 'green');
+},
+function() {
+	$('#o').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('p', function(){
+	$('#p').css('background', 'green');
+},
+function() {
+	$('#p').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('q', function(){
+	$('#q').css('background', 'green');
+},
+function() {
+	$('#q').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('r', function(){
+	$('#r').css('background', 'green');
+},
+function() {
+	$('#r').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('s', function(){
+	$('#s').css('background', 'green');
+},
+function() {
+	$('#s').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('t', function(){
+	$('#t').css('background', 'green');
+},
+function() {
+	$('#t').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('u', function(){
+	$('#u').css('background', 'green');
+},
+function() {
+	$('#u').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('v', function(){
+	$('#v').css('background', 'green');
+},
+function() {
+	$('#v').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('w', function(){
+	$('#w').css('background', 'green');
+},
+function() {
+	$('#w').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('x', function(){
+	$('#x').css('background', 'green');
+},
+function() {
+	$('#x').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('y', function(){
+	$('#y').css('background', 'green');
+},
+function() {
+	$('#y').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('z', function(){
+	$('#z').css('background', 'green');
+},
+function() {
+	$('#z').css('background', '#ffffff');
+}
+);
+
+// Uppercase Alphabet
+KeyboardJS.on('shift + a', function(){
+	$('#a').css('background', 'green');
+},
+function() {
+	$('#a').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + b', function(){
+	$('#b').css('background', 'green');
+},
+function() {
+	$('#b').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + c', function(){
+	$('#c').css('background', 'green');
+},
+function() {
+	$('#c').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + d', function(){
+	$('#d').css('background', 'green');
+},
+function() {
+	$('#d').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + e', function(){
+	$('#e').css('background', 'green');
+},
+function() {
+	$('#e').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + f', function(){
+	$('#f').css('background', 'green');
+},
+function() {
+	$('#f').css('background', '#ffffff');
+}
+);
+
+
+KeyboardJS.on('shift + g', function(){
+	$('#g').css('background', 'green');
+},
+function() {
+	$('#g').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + h', function(){
+	$('#h').css('background', 'green');
+},
+function() {
+	$('#h').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + i', function(){
+	$('#i').css('background', 'green');
+},
+function() {
+	$('#i').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + j', function(){
+	$('#j').css('background', 'green');
+},
+function() {
+	$('#j').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + k', function(){
+	$('#k').css('background', 'green');
+},
+function() {
+	$('#k').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + l', function(){
+	$('#l').css('background', 'green');
+},
+function() {
+	$('#l').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + m', function(){
+	$('#m').css('background', 'green');
+},
+function() {
+	$('#m').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + n', function(){
+	$('#n').css('background', 'green');
+},
+function() {
+	$('#n').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + o', function(){
+	$('#o').css('background', 'green');
+},
+function() {
+	$('#o').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + p', function(){
+	$('#p').css('background', 'green');
+},
+function() {
+	$('#p').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + q', function(){
+	$('#q').css('background', 'green');
+},
+function() {
+	$('#q').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + r', function(){
+	$('#r').css('background', 'green');
+},
+function() {
+	$('#r').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + s', function(){
+	$('#s').css('background', 'green');
+},
+function() {
+	$('#s').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + t', function(){
+	$('#t').css('background', 'green');
+},
+function() {
+	$('#t').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + u', function(){
+	$('#u').css('background', 'green');
+},
+function() {
+	$('#u').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + v', function(){
+	$('#v').css('background', 'green');
+},
+function() {
+	$('#v').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + w', function(){
+	$('#w').css('background', 'green');
+},
+function() {
+	$('#w').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + x', function(){
+	$('#x').css('background', 'green');
+},
+function() {
+	$('#x').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + y', function(){
+	$('#y').css('background', 'green');
+},
+function() {
+	$('#y').css('background', '#ffffff');
+}
+);
+
+KeyboardJS.on('shift + z', function(){
+	$('#z').css('background', 'green');
+},
+function() {
+	$('#z').css('background', '#ffffff');
+}
+);
+
+// Numbers and top row characters
+KeyboardJS.on('`', function(){
+	$('#tildeq').css('background', 'green');
+},
+function() {
+	$('#tildeq').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + `', function(){
+	$('#tildeq').css('background', 'green');
+},
+function() {
+	$('#tildeq').css('background', '#ffffff');
+});
+
+KeyboardJS.on('1', function(){
+	$('#exclaim1').css('background', 'green');
+},
+function() {
+	$('#exclaim1').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 1', function(){
+	$('#exclaim1').css('background', 'green');
+},
+function() {
+	$('#exclaim1').css('background', '#ffffff');
+});
+
+KeyboardJS.on('2', function(){
+	$('#at2').css('background', 'green');
+},
+function() {
+	$('#at2').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 2', function(){
+	$('#at2').css('background', 'green');
+},
+function() {
+	$('#at2').css('background', '#ffffff');
+});
+
+KeyboardJS.on('3', function(){
+	$('#hash3').css('background', 'green');
+},
+function() {
+	$('#hash3').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 3', function(){
+	$('#hash3').css('background', 'green');
+},
+function() {
+	$('#hash3').css('background', '#ffffff');
+});
+
+KeyboardJS.on('4', function(){
+	$('#dollar4').css('background', 'green');
+},
+function() {
+	$('#dollar4').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 4', function(){
+	$('#dollar4').css('background', 'green');
+},
+function() {
+	$('#dollar4').css('background', '#ffffff');
+});
+
+KeyboardJS.on('5', function(){
+	$('#perc5').css('background', 'green');
+},
+function() {
+	$('#perc5').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 5', function(){
+	$('#perc5').css('background', 'green');
+},
+function() {
+	$('#perc5').css('background', '#ffffff');
+});
+
+KeyboardJS.on('6', function(){
+	$('#up6').css('background', 'green');
+},
+function() {
+	$('#up6').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 6', function(){
+	$('#up6').css('background', 'green');
+},
+function() {
+	$('#up6').css('background', '#ffffff');
+});
+
+KeyboardJS.on('7', function(){
+	$('#and7').css('background', 'green');
+},
+function() {
+	$('#and7').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 7', function(){
+	$('#and7').css('background', 'green');
+},
+function() {
+	$('#and7').css('background', '#ffffff');
+});
+
+KeyboardJS.on('8', function(){
+	$('#aster8').css('background', 'green');
+},
+function() {
+	$('#aster8').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 8', function(){
+	$('#aster8').css('background', 'green');
+},
+function() {
+	$('#aster8').css('background', '#ffffff');
+});
+
+KeyboardJS.on('9', function(){
+	$('#openparen9').css('background', 'green');
+},
+function() {
+	$('#openparen9').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 9', function(){
+	$('#openparen9').css('background', 'green');
+},
+function() {
+	$('#openparen9').css('background', '#ffffff');
+});
+
+KeyboardJS.on('0', function(){
+	$('#closeparen0').css('background', 'green');
+},
+function() {
+	$('#closeparen0').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + 0', function(){
+	$('#closeparen0').css('background', 'green');
+},
+function() {
+	$('#closeparen0').css('background', '#ffffff');
+});
+
+KeyboardJS.on('-', function(){
+	$('#underdash').css('background', 'green');
+},
+function() {
+	$('#underdash').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + -', function(){
+	$('#underdash').css('background', 'green');
+},
+function() {
+	$('#underdash').css('background', '#ffffff');
+});
+
+KeyboardJS.on('=', function(){
+	$('#plusequal').css('background', 'green');
+},
+function() {
+	$('#plusequal').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + =', function(){
+	$('#plusequal').css('background', 'green');
+},
+function() {
+	$('#plusequal').css('background', '#ffffff');
+});
+
+// Special Keys
+
+KeyboardJS.on('shift',function(){
+	$('.letter').toggleClass('uppercase');
+	$('.symbol span').toggle();
+	$('#left-shift').css('background','green');
+	$('#right-shift').css('background','green');
+},
+function(){
+	$('.letter').toggleClass('uppercase');
+	$('.symbol span').toggle();
+	$('#left-shift').css('background','#ffffff');
+	$('#right-shift').css('background','#ffffff');
+});
+
+KeyboardJS.on('capslock',function(){
+	$('.capslock').css('background','green');
+	$('.letter').toggleClass('uppercase');
+}, function(){
+	$('.capslock').css('background','#ffffff');
+	$('.letter').toggleClass('uppercase');
+});
+
+KeyboardJS.on('[', function(){
+	$('#openbracket').css('background','green');
+},function(){
+	$('#openbracket').css('background','#ffffff');
+});
+KeyboardJS.on('shift + [', function(){
+	$('#openbracket').css('background','green');
+},function(){
+	$('#openbracket').css('background','#ffffff');
+});
+
+
+KeyboardJS.on(']', function(){
+	$('#closebracket').css('background','green');
+},function(){
+	$('#closebracket').css('background','#ffffff');
+});
+
+KeyboardJS.on('shift + ]', function(){
+	$('#closebracket').css('background','green');
+},function(){
+	$('#closebracket').css('background','#ffffff');
+});
+
+KeyboardJS.on('\\', function(){
+	$('#pipe').css('background','green');
+},function(){
+	$('#pipe').css('background','#ffffff');
+});
+KeyboardJS.on('shift + \\', function(){
+	$('#pipe').css('background','green');
+},function(){
+	$('#pipe').css('background','#ffffff');
+});
+
+KeyboardJS.on(';', function(){
+	$('#semicolon').css('background','green');
+},function(){
+	$('#semicolon').css('background','#ffffff');
+});
+KeyboardJS.on('shift + ;', function(){
+	$('#semicolon').css('background','green');
+},function(){
+	$('#semicolon').css('background','#ffffff');
+});
+
+KeyboardJS.on("'", function(){
+	$('#singlequote').css('background','green');
+},function(){
+	$('#singlequote').css('background','#ffffff');
+});
+KeyboardJS.on("shift + '", function(){
+	$('#singlequote').css('background','green');
+},function(){
+	$('#singlequote').css('background','#ffffff');
+});
+
+KeyboardJS.on('enter', function(){
+	$('#enter').css('background','green');
+},function(){
+	$('#enter').css('background','#ffffff');
+});
+
+
+// All posibilities for delete
+KeyboardJS.on('delete',function(){
+	$('#delete').css('background','green');
+},function(){
+	$('#delete').css('background','#ffffff');
+});
+
+KeyboardJS.on('del',function(){
+	$('#delete').css('background','green');
+},function(){
+	$('#delete').css('background','#ffffff');
+});
+
+KeyboardJS.on('backspace', function(){
+	$('#delete').css('background','green');
+}, function(){
+	$('#delete').css('background','#ffffff');
+});
