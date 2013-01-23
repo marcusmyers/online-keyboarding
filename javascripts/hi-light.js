803de36d7751cce1eadc28abb673235157532c1e
Mark Myers <marcusmyers@gmail.com>
2013-01-22 23:49:59 -0500

    Added missing keys from keyboard

--- javascripts/hi-light.js
+++ javascripts/hi-light.js
@@ -687,6 +687,47 @@ KeyboardJS.on('enter', function(){
 	$('#enter').css('background','#ffffff');
 });
 
+KeyboardJS.on('comma', function(){
+	$('#commalt').css('background', 'green');
+}, function(){
+	$('#commalt').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + comma', function(){
+	$('#commalt').css('background', 'green');
+}, function(){
+	$('#commalt').css('background', '#ffffff');
+});
+
+KeyboardJS.on('period', function(){
+	$('#periodgt').css('background', 'green');
+}, function(){
+	$('#periodgt').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + period', function(){
+	$('#periodgt').css('background', 'green');
+}, function(){
+	$('#periodgt').css('background', '#ffffff');
+});
+
+KeyboardJS.on('/', function(){
+	$('#slashques').css('background', 'green');
+}, function(){
+	$('#slashques').css('background', '#ffffff');
+});
+
+KeyboardJS.on('shift + /', function(){
+	$('#slashques').css('background', 'green');
+}, function(){
+	$('#slashques').css('background', '#ffffff');
+});
+
+KeyboardJS.on('space', function(){
+	$('.space').css('background', 'green');
+}, function(){
+	$('.space').css('background', '#ffffff');
+});
 
 // All posibilities for delete
 KeyboardJS.on('delete',function(){
@@ -705,10 +746,4 @@ KeyboardJS.on('backspace', function(){
 	$('#delete').css('background','green');
 }, function(){
 	$('#delete').css('background','#ffffff');
-});
-
-KeyboardJS.on('space', function(){
-	$('.space').css('background', 'green');
-}, function(){
-	$('.space').css('background', '#ffffff');
 });
